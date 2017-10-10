273aa5e8860dee5f785d52f22e043a8bd4fd0252
Lukas Ruebbelke <simpul@gmail.com>
2017-10-10 05:20:16 -0500

    update: getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,12 @@
 import { Component } from '@angular/core';
-import { request } from 'graphql-request';
-
-const BASE_URL = 'http://localhost:3000/graphql';
-
-interface Student {
-  id: string;
-  firstName: string;
-  lastName: string;
-  active: boolean;
-  courses: Course[];
-}
-
-interface Course {
-  id: string;
-  name: string;
-  description: string;
-  level: string;
-}
-
-interface QueryResponse {
-  allStudents;
-}
-
-const AllStudentsQuery = `
-  query allStudents {
-    allStudents {
-      id
-      firstName
-      lastName
-      active
-    }
-  }
-`;
 
 @Component({
   selector: 'app-root',
   template: `
-        <h1>Students</h1>
-        <pre>{{students | json}}</pre>
+        <h1>{{title}}</h1>
     `,
   styles: []
 })
 export class AppComponent {
-  students: Student[];
-  constructor() {
-    request(BASE_URL, AllStudentsQuery).then(
-      (data: QueryResponse) => (this.students = data.allStudents)
-    );
-  }
+  title = 'Students';
 }
